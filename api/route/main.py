20584d667517eb1797ab95c78274c41ee52337bf
jahandaniyal <jahandaniyal@gmail.com>
2022-04-05 16:43:01 +0200

    Add view for popularity check functionality

--- api/route/main.py
+++ api/route/main.py
@@ -1,5 +1,7 @@
+from flask import Blueprint, jsonify, request
 from http import HTTPStatus
-from flask import Blueprint, jsonify
+
+from api.utils.github_helper import GitHubAPI, ResourceNotAvailable
 
 app_api = Blueprint('api', __name__)
 
@@ -12,3 +14,17 @@ def ping_server():
     ---
     """
     return jsonify({"status": "OK"}), HTTPStatus.OK
+
+
+@app_api.route('/stats/<owner>/<repo>', methods=['GET'])
+def github_repo_stats(owner, repo):
+    gitstats = GitHubAPI(owner, repo, request.headers.get('X-Github-Token'))
+    try:
+        gitstats.get_repo()
+        result, message = gitstats.is_popular()
+    except ResourceNotAvailable as rna:
+        return jsonify({"error": rna.message}), rna.status_code
+    except Exception as ex:
+        return jsonify({"error": ex.message}), HTTPStatus.INTERNAL_SERVER_ERROR
+    else:
+        return jsonify(message), HTTPStatus.OK
